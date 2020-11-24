ff3ef0f93b92b928dd361b461b29000c9b7980d2
sueliton oliveira <suelitondeoliveira@gmail.com>
2020-11-24 19:57:36 -0300

    Created

---
+++ src/main/java/br/com/suelitoncursopringboot/CursopringbootApplication.java
@@ -0,0 +1,13 @@
+package br.com.suelitoncursopringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursopringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursopringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/suelitoncursopringboot/CursopringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.suelitoncursopringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursopringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
