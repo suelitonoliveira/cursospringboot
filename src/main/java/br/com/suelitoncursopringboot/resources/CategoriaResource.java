c281cbe624b700eaeaa79e58bd763717f6e08bf4
sueliton oliveira <suelitondeoliveira@gmail.com>
2020-11-29 18:13:13 -0300

    Acrescentando bd h2 e jpa

--- src/main/java/br/com/suelitoncursopringboot/resources/CategoriaResource.java
+++ src/main/java/br/com/suelitoncursopringboot/resources/CategoriaResource.java
@@ -16,8 +16,8 @@ public class CategoriaResource {
 	@RequestMapping(method = RequestMethod.GET)
 	public List<Categoria>listar() {
 		
-		Categoria cat1 = new Categoria(1, "Informática");
-		Categoria cat2 = new Categoria(2, "Escritório");
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
 		
 		List<Categoria> lista =  new ArrayList<>();
 		lista.add(cat1);
