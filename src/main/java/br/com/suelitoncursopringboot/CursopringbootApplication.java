c2c574436de0765c5ed778d55c31910d3fd6ee61
suelitonoliveira <suelitondeoliveira@gmail.com>
2020-12-04 10:19:46 -0300

    Populando dados em Categoria

--- src/main/java/br/com/suelitoncursopringboot/CursopringbootApplication.java
+++ src/main/java/br/com/suelitoncursopringboot/CursopringbootApplication.java
@@ -1,13 +1,34 @@
 package br.com.suelitoncursopringboot;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.suelitoncursopringboot.domain.Categoria;
+import br.com.suelitoncursopringboot.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursopringbootApplication {
+public class CursopringbootApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursopringbootApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+	}
+	
+	
 
 }
