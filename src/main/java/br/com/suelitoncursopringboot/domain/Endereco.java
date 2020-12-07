61120695992a7f84f71a73164a890b4bc1db04d2
suelitonoliveira <suelitondeoliveira@gmail.com>
2020-12-07 18:55:14 -0300

    Cliente, TipoCliente,Endereco

---
+++ src/main/java/br/com/suelitoncursopringboot/domain/Endereco.java
@@ -0,0 +1,139 @@
+package br.com.suelitoncursopringboot.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import br.com.suelitoncursopringboot.domain.enums.TipoCliente;
+
+@Entity
+public class Endereco implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String logradouro;
+	private String numero;
+	private String complemento;
+	private TipoCliente bairro;
+	private TipoCliente cep;
+	@ManyToOne
+	@JoinColumn(name="cliente_id")
+	private Cliente cliente;
+	@ManyToOne
+	@JoinColumn(name="cidade_id")
+	private Cidade Cidade;
+
+	public Endereco() {
+
+	}
+
+	public Endereco(Integer id, String logradouro, String numero, String complemento, TipoCliente bairro,
+			TipoCliente cep, Cliente cliente, br.com.suelitoncursopringboot.domain.Cidade cidade) {
+		super();
+		this.id = id;
+		this.logradouro = logradouro;
+		this.numero = numero;
+		this.complemento = complemento;
+		this.bairro = bairro;
+		this.cep = cep;
+		this.cliente = cliente;
+		setCidade(cidade);
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getLogradouro() {
+		return logradouro;
+	}
+
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public String getComplemento() {
+		return complemento;
+	}
+
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+
+	public TipoCliente getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(TipoCliente bairro) {
+		this.bairro = bairro;
+	}
+
+	public TipoCliente getCep() {
+		return cep;
+	}
+
+	public void setCep(TipoCliente cep) {
+		this.cep = cep;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public Cidade getCidade() {
+		return Cidade;
+	}
+
+	public void setCidade(Cidade cidade) {
+		Cidade = cidade;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Endereco other = (Endereco) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+}
