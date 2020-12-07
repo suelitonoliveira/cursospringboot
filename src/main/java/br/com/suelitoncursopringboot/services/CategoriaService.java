3abaaaec62f7a08c501ced7b8ce91370d7aef209
suelitonoliveira <suelitondeoliveira@gmail.com>
2020-12-07 17:55:27 -0300

    Criado Exception personalizada

--- src/main/java/br/com/suelitoncursopringboot/services/CategoriaService.java
+++ src/main/java/br/com/suelitoncursopringboot/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import br.com.suelitoncursopringboot.domain.Categoria;
 import br.com.suelitoncursopringboot.repositories.CategoriaRepository;
+import br.com.suelitoncursopringboot.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,7 +17,10 @@ public class CategoriaService {
 
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		if (obj.isEmpty()) {
+			throw new  ObjectNotFoundException("Objeto não encontrato! ID: " + id + ", Tipo: " + Categoria.class.getName());
+		}
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Not found"));
 	}
 	
 }
