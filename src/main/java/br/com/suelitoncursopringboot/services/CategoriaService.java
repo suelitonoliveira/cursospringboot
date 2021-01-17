8c97cb3dd03a9d291e3ac07501728b720878f82f
sueliton oliveira <suelitondeoliveira@gmail.com>
2021-01-17 11:25:09 -0300

    Listando Todas Categorias com DTO

--- src/main/java/br/com/suelitoncursopringboot/services/CategoriaService.java
+++ src/main/java/br/com/suelitoncursopringboot/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package br.com.suelitoncursopringboot.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import javax.transaction.Transactional;
@@ -46,6 +47,10 @@ public class CategoriaService {
 			throw new DataIntegrityException("Não é possivel excluir uma categoria com produtos associados");
 		}
 
+	}
+	
+	public List<Categoria> findAll(){
+		return repo.findAll();
 	}
 	
 }
